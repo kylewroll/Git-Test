3e912b094b3525d43323b5579b001917d2ea262b
Kyle Rolland <kylwroll@ut.utm.edu>
2021-01-22 16:57:28 -0600

    Created simple number guessing game, with numbers between 0 and 100, records how many times user guesses and the relation between their guess and the correct number. Does not print out number generated that must be guessed.

--- G_Test/Program.cs
+++ G_Test/Program.cs
@@ -10,7 +10,51 @@ namespace G_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var num = new Random(); //Random function inspired from MSDN
+
+            int tries = 1;
+
+            Console.WriteLine("I am thinking of a number between 0 and 100");
+
+            int answer = num.Next(101); //.Next(101) taken from MSDN
+
+
+
+            while (tries != 0)
+            {
+                Console.WriteLine("Please enter your guess:");
+
+                int guess = Convert.ToInt32(Console.ReadLine()); //Convert.ToInt32 was taken from MSDN
+
+
+                
+                if(guess == answer)
+                {
+                    Console.WriteLine("Congratulations, you have guessed correctly, in " + tries + " tries!");
+
+                    Console.ReadKey();
+
+                    tries = 0;
+                }
+
+
+
+                else if (guess > answer)
+                {
+                    Console.WriteLine("Your guess was too high, try again!");
+
+                    tries++;
+                }
+
+
+
+                else
+                {
+                    Console.WriteLine("Your guess was too low, try again!");
+
+                    tries++;
+                }
+            }
         }
     }
 }
